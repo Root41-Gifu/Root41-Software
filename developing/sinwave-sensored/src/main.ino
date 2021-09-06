9fd87d4d190e32c8bc0b4ed74d9ef207ab27c7af
Shirokuma@胡瓜 <shirokuma89.git@icloud.com>
2021-09-06 17:04:33 +0900

    ちょいよき

--- developing/sinwave-sensored/src/main.ino
+++ developing/sinwave-sensored/src/main.ino
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include <FlexiTimer2.h>
 
-volatile int IN[3] = {3, 5, 7};
+int IN[3] = {3, 5, 7};
 int SD[3] = {9, 6, 8};
 
 float power = 1;
@@ -13,22 +13,15 @@ volatile int drive;
 volatile int _drive;
 volatile int velocity;
 
-volatile int surveyPwm[1024];
-volatile int interval = 0;
-volatile int interval2 = 0;
+int surveyPwm[1024];
+
+volatile uint8_t interval = 0;
+volatile uint8_t interval2 = 0;
 volatile int current;
 
 void measureAngularVelocity(void) {
   if (interval == 0) {
-    deg = (analogRead(A0) + offset + 22 + velocity / 7) % 1024;
-
-    if (abs(deg - _deg) <= 150 || abs(deg - _deg) >= 874) {
-      drive = deg;
-    } else {
-      drive = _deg;
-    }
-
-    _deg = deg;
+    drive = analogRead(A0);
   }
 
   if (interval2 == 0) {
@@ -36,19 +29,21 @@ void measureAngularVelocity(void) {
     if (velocity >= 512) {
       velocity = 1023 - velocity;
     }
+    _deg = velocity / 60;
     _drive = drive;
   }
 
-  current = (drive + ((interval * velocity) / 85)) % 1024;
+  current = drive + offset + int(velocity * 0.14) + interval * _deg;
 
   interval++;
   interval %= 12;
 
   interval2++;
-  interval2 %= 85;
+  interval2 %= 60;
 }
 
 void setup() {
+  // TIMSK0 = 0;
   initialize();
 
   calibration();
@@ -57,8 +52,10 @@ void setup() {
 
   digitalWrite(13, HIGH);
 
-  FlexiTimer2::set(1.0, 1.0 / (96 * (10 ^ 3)), measureAngularVelocity);
+  FlexiTimer2::set(1.0, 1.0 / (97 * (10 ^ 3)), measureAngularVelocity);
   FlexiTimer2::start();
+
+  offset += 27;
 
   for (int i = 0; i < 3; i++) {  // SD端子をHIGHにする（通電させる）
     digitalWrite(SD[i], HIGH);
@@ -67,7 +64,9 @@ void setup() {
 }
 
 void loop() {
-  OCR3C = byte(constrain(surveyPwm[current] * power, 0, 254));
-  OCR3A = byte(constrain(surveyPwm[(current + 98) % 1024] * power, 0, 254));
-  OCR4B = byte(constrain(surveyPwm[(current + 49) % 1024] * power, 0, 254));
+  while (1) {
+    OCR3C = byte(constrain(surveyPwm[current % 1024] * power, 0, 254));
+    OCR3A = byte(constrain(surveyPwm[(current + 98) % 1024] * power, 0, 254));
+    OCR4B = byte(constrain(surveyPwm[(current + 49) % 1024] * power, 0, 254));
+  }
 }
