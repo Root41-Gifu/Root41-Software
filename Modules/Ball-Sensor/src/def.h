2af78ff3b577cf520ddd56545448edd45d681960
Shirokuma@胡瓜 <shirokuma89.git@icloud.com>
2021-10-05 13:07:39 +0900

    SPI通信ができることを確認。

---
+++ Modules/Ball-Sensor/src/def.h
@@ -0,0 +1,8 @@
+//ボール
+#define BALL_RST PD0
+
+//SPI関連
+#define SS_PIN 10
+#define SCK_PIN 13
+#define MISO_PIN 12
+#define MOSI_PIN 11
--- Modules/Ball-Sensor/src/main.cpp
+++ Modules/Ball-Sensor/src/main.cpp
@@ -1,14 +1,37 @@
 #include <Arduino.h>
-
-#define BALL_RST PD0
-
+#include <def.h>
+#include <SPI.h>
+volatile byte ch;
+ 
 void setup() {
-  pinMode(BALL_RST,OUTPUT);
+  pinMode(MISO,OUTPUT); //MISOを出力
+ 
+  SPI.setBitOrder(MSBFIRST);  //最上位ビット(MSB)から送信
+  SPI.setClockDivider(SPI_CLOCK_DIV64);  //通信速度をデフォルト
+  SPI.setDataMode(SPI_MODE2);   //アイドル5Vで0V→5Vの変化で送信する
+  SPCR |= _BV(SPE);
+     
+  SPI.attachInterrupt(); 
+}
+ 
+// SPI割り込み処理
+ISR(SPI_STC_vect)
+{
+  ch = SPDR;    //SPIの受信バッファから取得
+  SPDR = ch;  //送信バッファに書き込む
+}
+ 
+void loop() {
+  // put your main code here, to run repeatedly:
 }
 
-void loop() {
-  digitalWrite(BALL_RST,HIGH);
-  delay(2000);
-  digitalWrite(BALL_RST,LOW);
-  delay(2000);
-}
+// void setup() {
+//   pinMode(BALL_RST,OUTPUT);
+// }
+
+// void loop() {
+//   digitalWrite(BALL_RST,HIGH);
+//   delay(2000);
+//   digitalWrite(BALL_RST,LOW);
+//   delay(2000);
+// }
