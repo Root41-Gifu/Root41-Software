6f1045531d7aa206975dab419152ea2a2cf6297c
Shirokuma@胡瓜 <shirokuma89.git@icloud.com>
2021-11-24 12:02:26 +0900

    余計なものを消してシンプルにした

--- Program Offence A/src/main.ino
+++ Program Offence A/src/main.ino
@@ -47,52 +47,9 @@ Adafruit_NeoPixel rear(LED_REAR, LED_PIN_B, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel left(LED_LEFT, LED_PIN_L, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel right(LED_RIGHT, LED_PIN_R, NEO_GRB + NEO_KHZ800);
 
+//モーターのやつ
 HardwareSerial Serial4(A1, A0);
 HardwareSerial Serial1(PA10, PA9);
-
-int IN[3] = {3, 5, 7};
-int SD[3] = {9, 6, 8};
-
-float power = 0.2;
-
-volatile int offset = 0;
-volatile int velocityUnit;
-volatile int deg;
-volatile int oldDeg;
-volatile int velocity;
-
-int pwmRaw[360] = {
-    237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 246, 247, 248, 248, 249,
-    250, 250, 251, 251, 252, 252, 252, 253, 253, 253, 254, 254, 254, 254, 254,
-    254, 254, 254, 254, 254, 254, 253, 253, 253, 252, 252, 252, 251, 251, 250,
-    250, 249, 248, 248, 247, 246, 246, 245, 244, 243, 242, 241, 240, 239, 238,
-    237, 234, 230, 227, 223, 220, 216, 213, 209, 206, 202, 199, 195, 191, 188,
-    184, 180, 176, 173, 169, 165, 161, 158, 154, 150, 146, 142, 139, 135, 131,
-    127, 123, 119, 115, 112, 108, 104, 100, 96,  93,  89,  85,  81,  78,  74,
-    70,  66,  63,  59,  55,  52,  48,  45,  41,  38,  34,  31,  27,  24,  20,
-    17,  16,  15,  14,  13,  12,  11,  10,  9,   8,   8,   7,   6,   6,   5,
-    4,   4,   3,   3,   2,   2,   2,   1,   1,   1,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   1,   1,   1,   2,   2,   2,   3,   3,   4,
-    4,   5,   6,   6,   7,   8,   8,   9,   10,  11,  12,  13,  14,  15,  16,
-    17,  16,  15,  14,  13,  12,  11,  10,  9,   8,   8,   7,   6,   6,   5,
-    4,   4,   3,   3,   2,   2,   2,   1,   1,   1,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   1,   1,   1,   2,   2,   2,   3,   3,   4,
-    4,   5,   6,   6,   7,   8,   8,   9,   10,  11,  12,  13,  14,  15,  16,
-    17,  20,  24,  27,  31,  34,  38,  41,  45,  48,  52,  55,  59,  63,  66,
-    70,  74,  78,  81,  85,  89,  93,  96,  100, 104, 108, 112, 115, 119, 123,
-    127, 131, 135, 139, 142, 146, 150, 154, 158, 161, 165, 169, 173, 176, 180,
-    184, 188, 191, 195, 199, 202, 206, 209, 213, 216, 220, 223, 227, 230, 234,
-    237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 246, 247, 248, 248, 249,
-    250, 250, 251, 251, 252, 252, 252, 253, 253, 253, 254, 254, 254, 254, 254,
-    254, 254, 254, 254, 254, 254, 253, 253, 253, 252, 252, 252, 251, 251, 250,
-    250, 249, 248, 248, 247, 246, 246, 245, 244, 243, 242, 241, 240, 239, 238,
-};
-
-byte pwm[1024];
-
-volatile uint8_t interval = 0;
-
-volatile float gain = 0.114;
 
 float Battery;
 int MotorPower = 100;
@@ -283,19 +240,6 @@ class _gyro {
   int offsetVal;
 } gyro;
 
-// class _motor {
-//  public:
-//   void begin(void);
-//   void directDrive(int *v);
-
-//   int v[4] = {0, 0, 0, 0};
-
-//  private:
-// } motor;
-
-// void measureAngularVelocity(void) {
-//   deg = analogRead(A0);
-
 SPISettings MAX6675Setting(4000000, MSBFIRST, SPI_MODE0);
 
 void setup() {
@@ -459,26 +403,21 @@ void loop() {
     emergency = true;
   }
 
-  // // Serial---------------------------------------------
-  // Serial.print(motorPS(5));
-  // Serial.print(" ");
-
-  // Serial.println(" ");
   Serial.println(gyro.deg);
 }
 
-int motorPS(int value) {
-  // XY------
-  byte sendValue;
-  bool rotation;
-  if (value > 0) {
-    rotation = true;
-  } else if (value < 0) {
-    rotation = false;
-  }
-  byte absValue = abs(value);
-  if (value <= 61) {
-    sendValue = (rotation << 6) | (absValue);
-  }
-  return sendValue;
-}
+// int motorPS(int value) {
+//   // XY------
+//   byte sendValue;
+//   bool rotation;
+//   if (value > 0) {
+//     rotation = true;
+//   } else if (value < 0) {
+//     rotation = false;
+//   }
+//   byte absValue = abs(value);
+//   if (value <= 61) {
+//     sendValue = (rotation << 6) | (absValue);
+//   }
+//   return sendValue;
+// }
