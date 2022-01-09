9d60791859e0e491ff8f412dd533d7606cec5d9d
shirokuma-89 <shirokuma89.git@icloud.com>
2022-01-09 16:24:52 +0900

    機械角と電気角の処理を実装。コメントなどもがんばた（）

--- ESC-LL001/Temp/ESC.cpp
+++ ESC-LL001/Temp/ESC.cpp
@@ -1,4 +1,4 @@
-//よく使う数学的関数やマクロ
+//---------------よく使う数学的関数やマクロ---------------
 #define constrain(amt, low, high) \
   ((amt) < (low) ? (low) : ((amt) > (high) ? (high) : (amt)))
 
@@ -6,31 +6,41 @@ extern long map(long x, long in_min, long in_max, long out_min, long out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-// 割り込み関連の変数。インターバル時間何度を監視する。
-int interval = 1000;
-int intervalCounter = 0;
-
-//パラメータなど。キャリブレーション処理をして測定する。とりあえずダミーで0を入力しておく。
-
+//---------------動的パラメータ。キャリブレーション処理などで決定する。FLASH領域にあらかじめ書き込んでおくのが理想。---------------
 int mechanicalAngleOffset = 0;
 
-// ここはinterval[μs]秒ごとに実行される
-void INTERVAL(void) {}
+//---------------固定パラメータ。メンテナンスを容易にするために定数として定義しておく。---------------
+const int outputVoltageLimit = 200;  //出力電圧のリミット。0-255の間で指定。
+const int interval = 1000;  //割り込み時間間隔の設定。単位はμs
+const int intervalCounterLimit = 10;  //間引き用実行処理間隔指定
 
-int getAngularVelocity(void) {}
+//---------------モーターとは直接関わりのない変数。---------------
+int intervalCounter = 0;  //間引いて実行したい時用のインターバルカウンタ。
 
-//現在ロータ位置の取得 0-4095を電気角に変換して0-127でreturnする。
-int getAngle(void) {
+//---------------関数---------------
+
+void INTERVAL(void) {  // ここはinterval[μs]秒ごとに実行される
+}
+
+int getAngularVelocity(void) {  // 角速度を求める。Δtが一定であることが動作条件
+}
+
+int getMechanicalAngle(void) {  //現在ロータ位置の取得 0-4095でreturnされる
   int mechanicalAngle = 1234;  // LLでアナログ値の取得を行う。1234は仮の値
 
-  //オフセット処理
+  //オフセット処理（検出用磁石が電気角0度に向いているとは限らない）
   mechanicalAngle += 4096;
   mechanicalAngle += mechanicalAngleOffset;
   mechanicalAngle %= 4096;
 
-  //電気角に変換
-  int electricalAngle = constrain(mechanicalAngle, 0, 4095) % 585;
+  return mechanicalAngle;
+}
+
+int convertToElectricalAngle(const int _mechanicalAngle) {  //電気角に変換
+  int electricalAngle = constrain(_mechanicalAngle, 0, 4095) % 585;
   electricalAngle = constrain(map(electricalAngle, 0, 585, 0, 127), 0, 127);
 
   return electricalAngle;
 }
+
+void drive(int _electricalAngle, int power) {}
